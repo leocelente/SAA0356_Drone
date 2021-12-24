fb50ae2bf70c926017c6936c9dd22644f5be1445
Leonardo Celente <leocelente@gmail.com>
2021-12-23 23:09:25 -0300

    Added minimal (not fully tested) PWM sysfs driver interface

--- src/pwm_ctrl/pwm_driver_api/driver.c
+++ src/pwm_ctrl/pwm_driver_api/driver.c
@@ -1,1 +1,95 @@
-int driver_test() { return 1; }
+/**
+ * @file driver.c
+ * @author Leonardo Celente (@leocelente)
+ *
+ * @version 0.1
+ * @date 2021-12-23
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include "driver.h"
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+pwm_e_t pwm_init(pwm_channel_t *channel, char const *path,
+                 duty_cycle_t duty_cycle, period_t period) {
+
+  if (duty_cycle < 0.f || duty_cycle > 100.f) {
+    printf("Duty Cycle must be 0...100\n");
+    return PWM_FAIL;
+  }
+
+  if (period < 0) {
+    printf("Period must be positive!\n");
+    return PWM_FAIL;
+  }
+  channel->path = path;
+  pwm_set_period(channel, period);
+  channel->period = period;
+  pwm_set_duty_cycle(channel, duty_cycle);
+  channel->duty_cycle = duty_cycle;
+  return 0;
+}
+
+pwm_e_t pwm_set_duty_cycle(pwm_channel_t *channel, duty_cycle_t duty_cycle) {
+  char path[PATH_SIZE] = {0};
+  strcpy(path, channel->path);
+  strcat(path, "/duty_cycle");
+  int d = open(path, O_RDWR);
+  if (d < 0) {
+    printf("Failed to open file!\r\n");
+    return 1;
+  }
+  uint32_t dt_ns = ((duty_cycle * 1000.f) / 100.f) * channel->period;
+  char buffer[24] = {0};
+  sprintf(buffer, "%d", dt_ns);
+  if (write(d, buffer, strlen(buffer)) < 0) {
+    close(d);
+    printf("Failed to write to file\r\n");
+    return 2;
+  }
+  channel->duty_cycle = duty_cycle;
+  close(d);
+  printf("Setting PWM Duty Cycle to: %d ns\r\n", dt_ns);
+  return 0;
+}
+
+pwm_e_t pwm_set_period(pwm_channel_t *channel, period_t period) {
+  char path[PATH_SIZE];
+  strcpy(path, channel->path);
+  strcat(path, "/period");
+  int d = open(path, O_RDWR);
+  if (d < 0) {
+    printf("Failed to open file!\r\n");
+    return 1;
+  }
+  uint32_t period_ns = period * 1000;
+  char buffer[24] = {0};
+  sprintf(buffer, "%d", period_ns);
+  printf("Writing: %s to file\r\n", buffer);
+  if (write(d, buffer, strlen(buffer)) < 0) {
+    close(d);
+    printf("Failed to write to file\r\n");
+    return 2;
+  }
+  channel->period = period;
+  close(d);
+  printf("Setting PWM Period to :%d ns\r\n", period_ns);
+  return 0;
+}
+
+pwm_e_t pwm_enable(pwm_channel_t *channel) {
+  // TODO: implement
+  return 1;
+}
+
+pwm_e_t pwm_disable(pwm_channel_t *channel) {
+  // TODO: implement
+  return 1;
+}
--- src/pwm_ctrl/pwm_driver_api/driver.h
+++ src/pwm_ctrl/pwm_driver_api/driver.h
@@ -1,3 +1,54 @@
 #pragma once
+#include <stdbool.h>
+#include <stdint.h>
+#define PATH_SIZE 1024
+typedef float duty_cycle_t;
+typedef uint32_t period_t;
+typedef int file_descriptor_t;
 
-int driver_test();
+// Error type
+typedef enum { PWM_OK = 0, PWM_FAIL = 1 } pwm_e_t;
+
+typedef struct {
+  duty_cycle_t duty_cycle;
+  period_t period;
+  bool enabled;
+  char const *path;
+} pwm_channel_t;
+
+typedef struct {
+  uint8_t chip;
+  uint8_t channel;
+} fs_structure_t;
+
+/**
+    Changes channel duty cycle to given _percentage_
+    `channel`: PWM Channel
+    `duty`:_cycle Percentage duty cycle
+    @return error
+*/
+pwm_e_t pwm_set_duty_cycle(pwm_channel_t *channel, duty_cycle_t duty_cycle);
+
+/**
+    Changes PWM channel period
+    `channel`: PWM Channel
+    `period`: Period time in milliseconds
+    @return error
+*/
+pwm_e_t pwm_set_period(pwm_channel_t *channel, period_t period);
+
+pwm_e_t pwm_enable(pwm_channel_t *channel);
+pwm_e_t pwm_disable(pwm_channel_t *channel);
+
+/**
+ *  Initializes PWM channel in `path` with `duty_cycle` and
+ `period`
+ *
+ * `channel`:
+ * `path`: to sysfs char device
+ * `duty`:_cycle in percent
+ * `period`: in milliseconds
+ * @return pwm_e_t error
+ */
+pwm_e_t pwm_init(pwm_channel_t *channel, char const *path,
+                 duty_cycle_t duty_cycle, period_t period);
