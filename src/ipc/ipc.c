d3f5712045f9971f8057da679f2e1b0a262b8127
Leonardo Celente <leocelente@gmail.com>
2021-12-23 23:08:27 -0300

    Updated IPC interface

--- src/ipc/ipc.c
+++ src/ipc/ipc.c
@@ -1,2 +1,12 @@
 #include "ipc.h"
-int ipc_test() { return 1; }
+
+ipc_e_t ipc_client_init(ipc_client_t *client, ipc_connection_t *connection) {}
+
+ipc_e_t ipc_server_init(ipc_server_t *server) {}
+
+ipc_e_t ipc_client_send(ipc_client_t *client, ipc_msg_t const *message) {}
+
+ipc_e_t ipc_server_send(ipc_server_t *server, ipc_msg_t const *message) {}
+
+ipc_e_t ipc_server_listen(ipc_server_t *server, ipc_connection_t *connection,
+                          ipc_callback callback) {}
--- src/ipc/ipc.h
+++ src/ipc/ipc.h
@@ -1,3 +1,38 @@
 #pragma once
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int ipc_test();
+#define IPC_MSG_SIZE 255
+
+// error
+typedef enum { IPC_OK = 0, IPC_FAIL = 1 } ipc_e_t;
+
+typedef enum { IPC_CLIENT, IPC_SERVER } ipc_source_t;
+typedef struct {
+  ipc_source_t source;
+  uint8_t data[IPC_MSG_SIZE];
+} ipc_msg_t;
+
+typedef struct {
+} ipc_connection_t;
+
+typedef void (*ipc_callback)(ipc_msg_t const *);
+typedef struct {
+  ipc_connection_t connection;
+} ipc_client_t;
+
+typedef struct {
+  ipc_callback callback;
+  ipc_connection_t connection;
+} ipc_server_t;
+
+ipc_e_t ipc_client_init(ipc_client_t *client, ipc_connection_t *connection);
+
+ipc_e_t ipc_server_init(ipc_server_t *server);
+
+ipc_e_t ipc_client_send(ipc_client_t *client, ipc_msg_t const *message);
+
+ipc_e_t ipc_server_send(ipc_server_t *server, ipc_msg_t const *message);
+
+ipc_e_t ipc_server_listen(ipc_server_t *server, ipc_connection_t *connection,
+                          ipc_callback callback);
