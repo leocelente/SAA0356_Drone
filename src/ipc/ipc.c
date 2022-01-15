d21a647278877563875f2ebcedf559550955a543
Leonardo Celente <leocelente@gmail.com>
2022-01-15 12:22:14 -0300

    Simplified IPC module, using domain sockets

--- src/ipc/ipc.c
+++ src/ipc/ipc.c
@@ -1,111 +1,89 @@
+/**
+ * @file ipc.c
+ * @author Leonardo Celente (@leocelente)
+ *
+ * @version 0.1
+ * @date 2022-01-15
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include "ipc.h"
-#include <string.h>
 
-// http://www.cse.yorku.ca/~oz/hash.html
-static uint32_t hash(char *str) {
-  uint32_t hash = 5381;
-  char c = *str;
-  while (c != '\0') {
-    hash = ((hash << 5) + hash) + c;
-    c = *str++;
+ipc_e_t ipc_init(ipc_node_t *node, int side) {
+  node->out.path = !(side > 0) ? LEFT_SOCKET_NAME : RIGHT_SOCKET_NAME;
+  node->in.path = !(side > 0) ? RIGHT_SOCKET_NAME : LEFT_SOCKET_NAME;
+
+  printf("Input Socket: %s\r\nOutput Socket: %s\r\n", node->in.path,
+         node->out.path);
+
+  // In case of forced termination, reset socket before re-creation
+  unlink(node->in.path);
+
+  int in_sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (in_sock_fd < 0) {
+    perror("socket (input)");
+    return -3;
   }
-  return 4000 + (hash % 1000);
-}
+  node->in.fd = in_sock_fd;
 
-ipc_e_t ipc_init(ipc_node_t *node, int side, char *url) {
-  printf("Initializing Sockets\r\n");
-  int client_port = hash(url);
-  int server_port = client_port + 1;
+  node->in.paddr = (struct sockaddr *)&node->in.addr;
+  node->in.addrlen = sizeof(node->in.addr);
+  node->in.addr.sun_family = AF_UNIX;
+  strncpy(node->in.addr.sun_path, node->in.path,
+          sizeof(node->in.addr.sun_path) - 1);
 
-  if (side > 0) {
-    server_port = hash(url);
-    client_port = server_port + 1;
-  }
-
-  printf("Server: %d and Client: %d\r\n", server_port, client_port);
-
-  int const server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket_fd == 0) {
-    printf("Failed to Create to Server\r\n");
-    return 3;
-  }
-  struct sockaddr_in server_address;
-  int server_addrlen = sizeof(server_address);
-  server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = INADDR_ANY;
-  server_address.sin_port = htons(server_port);
-
-  int const sb = bind(server_socket_fd, (struct sockaddr *)&server_address,
-                      sizeof(server_address));
-  if (sb < 0) {
-    printf("Failed to Bind to Server (%d)\r\n", server_port);
-    return 4;
+  int binded = bind(node->in.fd, node->in.paddr, node->in.addrlen);
+  if (binded < 0) {
+    perror("bind (input)");
+    return -4;
   }
 
-  node->server.fd = server_socket_fd;
-  node->server.addrlen = server_addrlen;
-  node->server.paddr = (struct sockaddr *)&server_address;
-  node->client.port = client_port;
-  node->server.port = server_port;
-
-  printf("OK\r\n");
-  return 0;
-}
-
-ipc_e_t ipc_listen(ipc_node_t *node, ipc_callback handler) {
-  printf("Listenning... (%d)", node->server.fd);
-  int const listening = listen(node->server.fd, 2);
+  int listening = listen(node->in.fd, 2);
   if (listening < 0) {
-    printf("Failed  to LISTEN on socket\r\n");
-    return 1;
-  }
-  while (true) {
-    printf("...");
-    int const sock_client =
-        accept(node->server.fd, node->server.paddr, &node->server.addrlen);
-    if (sock_client < 0) {
-      printf("Socket ACCEPT failed (%d)\r\n", node->server.fd);
-      return 2;
-    }
-
-    char buffer[IPC_MSG_SIZE] = {0};
-    int valread = read(sock_client, buffer, IPC_MSG_SIZE);
-    ipc_msg_t msg;
-    memcpy(msg.data, buffer, sizeof(buffer));
-    msg.size = valread;
-    handler(&msg);
+    perror("listen (input)");
+    return -1;
   }
   return 0;
 }
 
-ipc_e_t ipc_send(ipc_node_t *node, ipc_msg_t *msg) {
-
-  printf("Sending [%d] to (%d)...\r\n", msg->size, node->client.fd);
-  int const client_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (client_socket_fd == 0) {
-    printf("Failed to Create to Client\r\n");
-    return 1;
-  }
-  struct sockaddr_in client_address;
-  int client_addrlen = sizeof(client_address);
-  client_address.sin_family = AF_INET;
-  client_address.sin_port = htons(node->client.port);
-
-  static bool first_time = true;
-  if (first_time) {
-    printf("This is the first message\r\n");
-    node->client.addrlen = client_addrlen;
-    node->client.paddr = (struct sockaddr *)&client_address;
-    first_time = false;
+ipc_e_t ipc_recv(ipc_node_t *node, ipc_msg_t *msg) {
+  int sock_client = accept(node->in.fd, node->in.paddr, &node->in.addrlen);
+  if (sock_client < 0) {
+    perror("accept (input)");
+    return -2;
   }
 
-  int const cb = connect(client_socket_fd, (struct sockaddr *)&client_address,
-                         sizeof(client_address));
-  if (cb < 0) {
-    printf("Failed to CONNECT to Client (%d)(%d)\r\n", cb, node->client.port);
-    return 2;
+  char buffer[IPC_MSG_SIZE] = {0};
+  int valread = recv(sock_client, buffer, IPC_MSG_SIZE, 0);
+  memcpy(msg->data, buffer, sizeof(buffer));
+  msg->size = valread;
+  return 0;
+}
+
+ipc_e_t ipc_send(ipc_node_t *node, ipc_msg_t const *msg) {
+  int out_sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (out_sock_fd < 0) {
+    perror("socket (output)");
+    return -1;
   }
-  node->client.fd = client_socket_fd;
-  send(node->client.fd, msg->data, sizeof(msg->data), 0);
+  node->out.fd = out_sock_fd;
+
+  // write data in struct for easier debugging
+  node->out.paddr = (struct sockaddr *)&node->out.addr;
+  node->out.addrlen = sizeof(node->out.addr);
+  node->out.addr.sun_family = AF_UNIX;
+  strncpy(node->out.addr.sun_path, node->out.path,
+          sizeof(node->out.addr.sun_path) - 1);
+
+  int connected = connect(node->out.fd, node->out.paddr, node->out.addrlen);
+  if (connected < 0) {
+    perror("connect (output)");
+    return -3;
+  }
+
+  send(node->out.fd, msg->data, sizeof(msg->data), 0);
+  close(node->out.fd);
   return 0;
 }
--- src/ipc/ipc.h
+++ src/ipc/ipc.h
@@ -1,36 +1,70 @@
 #pragma once
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #define IPC_MSG_SIZE 255
+#define LEFT_SOCKET_NAME "/tmp/left.sock"
+#define RIGHT_SOCKET_NAME "/tmp/right.sock"
 
 // error
-typedef enum { IPC_OK = 0, IPC_FAIL = 1 } ipc_e_t;
-
-typedef enum { IPC_CLIENT, IPC_SERVER } ipc_role_t;
+typedef enum { IPC_OK = 0, IPC_FAIL = -1 } ipc_e_t;
 
 typedef struct {
   int size;
   uint8_t data[IPC_MSG_SIZE];
 } ipc_msg_t;
 
-typedef void (*ipc_callback)(ipc_msg_t const *);
-
 typedef struct {
+  /* socket file descritor*/
   int fd;
+  /* socket address struct */
+  struct sockaddr_un addr;
+  /* pointer to `addr`*/
   struct sockaddr *paddr;
+  /* length of `addr`*/
   uint32_t addrlen;
-  int port;
+  /* name of the domain socket */
+  char *path;
 } ipc_connection_t;
 
 typedef struct {
-  ipc_connection_t server;
-  ipc_connection_t client;
+  /* input connection,
+    acts as server listening for clients */
+  ipc_connection_t in;
+  /* output connection,
+      acts as socket client */
+  ipc_connection_t out;
 } ipc_node_t;
 
-ipc_e_t ipc_init(ipc_node_t *node, int side, char *url);
-ipc_e_t ipc_listen(ipc_node_t *node, ipc_callback callback);
-ipc_e_t ipc_send(ipc_node_t *node, ipc_msg_t *msg);
+/**
+ * Initializes IPC Node
+ *
+ * `node`: pointer to node struct
+ * `side`: 0 or 1, sibling node must be other
+ * @return ipc_e_t error
+ */
+ipc_e_t ipc_init(ipc_node_t *node, int side);
+
+/**
+ * Blocking Received from other IPC node
+ *
+ * `node`: pointer to node
+ * `msg`: pointer to IPC message, to be filled
+ * @return ipc_e_t error
+ */
+ipc_e_t ipc_recv(ipc_node_t *node, ipc_msg_t *msg);
+
+/**
+ * Send IPC Message to other node
+ *
+ * `node`: pointer to node
+ * `msg`: message to be sent
+ * @return ipc_e_t error
+ */
+ipc_e_t ipc_send(ipc_node_t *node, ipc_msg_t const *msg);
